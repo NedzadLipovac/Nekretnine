e72357cdad2eae1a2e45942bbb4142749e0bc21b
Nedžad Lipovac <nedzad.lipovac@edu.fit.ba>
2020-01-18 13:31:00 +0100

    base controllers and services added

---
+++ Nekretnine.WebApi/Services/BaseCrudService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Nekretnine.WebApi.Database;
+
+namespace Nekretnine.WebApi.Services
+{
+    public class BaseCrudService<TModel, TSearch, TDatabase, TInsert, TUpdate> : BaseService<TModel, TSearch, TDatabase>, ICRUDService<TModel, TSearch, TInsert, TUpdate> where TDatabase : class
+    {
+        public BaseCrudService(NekretnineContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public TModel Insert(TInsert request)
+        {
+            var entity = _mapper.Map<TDatabase>(request);
+            _context.Set<TDatabase>().Add(entity);
+            _context.SaveChanges();
+            return _mapper.Map<TModel>(entity);
+        }
+
+        public TModel Update(int id, TUpdate request)
+        {
+            var entity = _context.Set<TDatabase>().Find(id);
+            _context.Set<TDatabase>().Attach(entity);
+            _context.Set<TDatabase>().Update(entity);
+            _mapper.Map(request, entity);
+            _context.SaveChanges();
+            return _mapper.Map<TModel>(entity);
+        }
+    }
+}
